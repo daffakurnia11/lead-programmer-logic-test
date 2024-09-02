8f47aec3bed715d3457be452f4049a51d7d8c75b
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-09-02 18:52:19 +0700

    docs: :memo: Add documentation for each logics

--- fishBash/fishBash.test.ts
+++ fishBash/fishBash.test.ts
@@ -2,26 +2,32 @@ import { fishBash } from './fishBash';
 
 const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
 
+// FishBash Function Tests
 describe('fishBash', () => {
+  // Reset console.log mock before each test
   beforeEach(() => {
     consoleLogSpy.mockClear();
   });
 
+  // Case : If number is divisible by 3
   it('should print "fish" for multiples of 3', () => {
     fishBash(3);
     expect(consoleLogSpy).toHaveBeenCalledWith('fish');
   });
 
+  // Case : If number is divisible by 5
   it('should print "bash" for multiples of 5', () => {
     fishBash(5);
     expect(consoleLogSpy).toHaveBeenCalledWith('bash');
   });
 
+  // Case : If number is divisible by both 3 and 5
   it('should print "fish bash" for multiples of 15', () => {
     fishBash(15);
     expect(consoleLogSpy).toHaveBeenCalledWith('fish bash');
   });
 
+  // Case : If number is not divisible by 3 nor 5
   it('should print numbers that are not multiples of 3 or 5', () => {
     fishBash(2);
     expect(consoleLogSpy).toHaveBeenCalledWith('1');
@@ -29,6 +35,7 @@ describe('fishBash', () => {
   });
 });
 
+// Reset console.log mock after all tests
 afterAll(() => {
   consoleLogSpy.mockRestore();
 });
--- fishBash/fishBash.ts
+++ fishBash/fishBash.ts
@@ -1,14 +1,16 @@
+// FishBash Function
 export function fishBash(num: number) {
+  // Looping from 1 to num
   for (let i = 1; i <= num; i++) {
     const print =
-      i % 3 === 0 && i % 5 === 0
+      i % 3 === 0 && i % 5 === 0 // If number is divisible by both 3 and 5
         ? "fish bash"
-        : i % 3 === 0
+        : i % 3 === 0 // If number is divisible by 3
         ? "fish"
-        : i % 5 === 0
+        : i % 5 === 0 // If number is divisible by 5
         ? "bash"
-        : i.toString();
-    console.log(print);
+        : i.toString(); // If number is not divisible by 3 nor 5
+    console.log(print); // Print the result
   }
 }
 
