bf489bd025a434a1fee8a5a6246cea75645ede7f
Daffa Kurnia Fatah <daffakurniaf11@gmail.com>
2024-09-02 19:23:39 +0700

    style: :rotating_light: Fix linting code

--- fishBash/fishBash.test.ts
+++ fishBash/fishBash.test.ts
@@ -1,9 +1,9 @@
-import { fishBash } from './fishBash';
+import { fishBash } from "./fishBash";
 
-const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
+const consoleLogSpy = jest.spyOn(console, "log").mockImplementation();
 
 // FishBash Function Tests
-describe('FishBash Test', () => {
+describe("FishBash Test", () => {
   // Reset console.log mock before each test
   beforeEach(() => {
     consoleLogSpy.mockClear();
@@ -12,26 +12,26 @@ describe('FishBash Test', () => {
   // Case : If number is divisible by 3
   it('should print "fish" for multiples of 3', () => {
     fishBash(3);
-    expect(consoleLogSpy).toHaveBeenCalledWith('fish');
+    expect(consoleLogSpy).toHaveBeenCalledWith("fish");
   });
 
   // Case : If number is divisible by 5
   it('should print "bash" for multiples of 5', () => {
     fishBash(5);
-    expect(consoleLogSpy).toHaveBeenCalledWith('bash');
+    expect(consoleLogSpy).toHaveBeenCalledWith("bash");
   });
 
   // Case : If number is divisible by both 3 and 5
   it('should print "fish bash" for multiples of 15', () => {
     fishBash(15);
-    expect(consoleLogSpy).toHaveBeenCalledWith('fish bash');
+    expect(consoleLogSpy).toHaveBeenCalledWith("fish bash");
   });
 
   // Case : If number is not divisible by 3 nor 5
-  it('should print numbers that are not multiples of 3 or 5', () => {
+  it("should print numbers that are not multiples of 3 or 5", () => {
     fishBash(2);
-    expect(consoleLogSpy).toHaveBeenCalledWith('1');
-    expect(consoleLogSpy).toHaveBeenCalledWith('2');
+    expect(consoleLogSpy).toHaveBeenCalledWith("1");
+    expect(consoleLogSpy).toHaveBeenCalledWith("2");
   });
 });
 
--- sort/sort.test.ts
+++ sort/sort.test.ts
@@ -1,31 +1,30 @@
-// sortFunctions.test.ts
-import { sort } from './sort';
+import { sort } from "./sort";
 
 // Sort Ascending Function Tests
-describe('Sort Ascending Test', () => {
+describe("Sort Ascending Test", () => {
   // Case : Sort an array of numbers in ascending order
-  it('should sort an array of numbers in ascending order', () => {
+  it("should sort an array of numbers in ascending order", () => {
     const input = [5, 2, 9, 1, 5, 6];
     const expectedOutput = [1, 2, 5, 5, 6, 9];
     expect(sort(input, "asc")).toEqual(expectedOutput);
   });
 
   // Case : Handle an empty array
-  it('should handle an empty array', () => {
+  it("should handle an empty array", () => {
     const input: number[] = [];
     const expectedOutput: number[] = [];
     expect(sort(input, "asc")).toEqual(expectedOutput);
   });
 
   // Case : Handle an array with one element
-  it('should handle an array with one element', () => {
+  it("should handle an array with one element", () => {
     const input = [1];
     const expectedOutput = [1];
     expect(sort(input, "asc")).toEqual(expectedOutput);
   });
 
   // Case : Handle an array with all identical elements
-  it('should handle an array with all identical elements', () => {
+  it("should handle an array with all identical elements", () => {
     const input = [4, 4, 4, 4];
     const expectedOutput = [4, 4, 4, 4];
     expect(sort(input, "asc")).toEqual(expectedOutput);
@@ -33,30 +32,30 @@ describe('Sort Ascending Test', () => {
 });
 
 // Sort Descending Function Tests
-describe('Sort Descending Test', () => {
+describe("Sort Descending Test", () => {
   // Case : Sort an array of numbers in descending order
-  it('should sort an array of numbers in descending order', () => {
+  it("should sort an array of numbers in descending order", () => {
     const input = [5, 2, 9, 1, 5, 6];
     const expectedOutput = [9, 6, 5, 5, 2, 1];
     expect(sort(input, "desc")).toEqual(expectedOutput);
   });
 
   // Case : Handle an empty array
-  it('should handle an empty array', () => {
+  it("should handle an empty array", () => {
     const input: number[] = [];
     const expectedOutput: number[] = [];
     expect(sort(input, "desc")).toEqual(expectedOutput);
   });
 
   // Case : Handle an array with one element
-  it('should handle an array with one element', () => {
+  it("should handle an array with one element", () => {
     const input = [1];
     const expectedOutput = [1];
     expect(sort(input, "desc")).toEqual(expectedOutput);
   });
 
   // Case : Handle an array with all identical elements
-  it('should handle an array with all identical elements', () => {
+  it("should handle an array with all identical elements", () => {
     const input = [4, 4, 4, 4];
     const expectedOutput = [4, 4, 4, 4];
     expect(sort(input, "desc")).toEqual(expectedOutput);
